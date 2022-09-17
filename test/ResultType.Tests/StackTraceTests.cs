adede659cc1b6d3d720690d6ef1dc071b7c5dca2
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-17 18:55:20 +0200

    :construction: Add StackTrace Tests

---
+++ test/ResultType.Tests/StackTraceTests.cs
@@ -0,0 +1,72 @@
+﻿namespace ResultType.Tests;
+
+public class StackTraceTests
+{
+    [Test]
+    public void StackTraceContainsThrowingMethodNameTest()
+    {
+        var result = Stack1();
+
+        var error = result.GetError();
+
+        Assert.That(error.StackTrace!.Contains(nameof(StackTraceTestInternal)));
+        Assert.That(error is InvalidOperationException);
+    }
+
+    [Test]
+    public void StackTraceContainsAllMethodNamesTest()
+    {
+        var result = RethrowStack1();
+
+        var error = result.GetError();
+
+        Assert.That(error.StackTrace!.Contains(nameof(StackTraceTestInternal)));
+        Assert.That(error.StackTrace!.Contains(nameof(RethrowStack1)));
+        Assert.That(error.StackTrace!.Contains(nameof(RethrowStack2)));
+        Assert.That(error is InvalidOperationException);
+    }
+
+    private Result RethrowStack1()
+    {
+        try
+        {
+            var result = RethrowStack2();
+            result.Check();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return ex;
+        }
+    }
+
+    private Result RethrowStack2()
+    {
+        try
+        {
+            var result = StackTraceTestInternal();
+            result.Check();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return ex;
+        }
+    }
+
+    private Result Stack1() => Stack2();
+
+    private Result Stack2() => StackTraceTestInternal();
+
+    private Result StackTraceTestInternal()
+    {
+        try
+        {
+            throw new InvalidOperationException("Test");
+        }
+        catch (Exception ex)
+        {
+            return ex;
+        }
+    }
+}
